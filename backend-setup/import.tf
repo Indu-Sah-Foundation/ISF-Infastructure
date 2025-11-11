89e95d23e18537f49cf3daf2fd8e60c922a5b276
exploratoryprorammer <rohan.k.sah@gmail.com>
2025-11-11 14:56:04 -0600

    temp

---
+++ backend-setup/import.tf
@@ -0,0 +1,17 @@
+# Import existing backend resources if they were created outside Terraform
+# Remove this file after first successful apply
+
+import {
+  to = azurerm_resource_group.state
+  id = "/subscriptions/e00ac8d0-d7ed-48ef-9463-cc5138d2a3db/resourceGroups/terraform-state-rg"
+}
+
+import {
+  to = azurerm_storage_account.state
+  id = "/subscriptions/e00ac8d0-d7ed-48ef-9463-cc5138d2a3db/resourceGroups/terraform-state-rg/providers/Microsoft.Storage/storageAccounts/isfterraformstate"
+}
+
+import {
+  to = azurerm_storage_container.state
+  id = "https://isfterraformstate.blob.core.windows.net/tfstate"
+}
