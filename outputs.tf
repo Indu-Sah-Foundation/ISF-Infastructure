1fa5554ea6ef177980988c3c4f7eea7fd7c9c1c5
exploratoryprorammer <rohan.k.sah@gmail.com>
2025-11-11 14:01:27 -0600

    updated infrastructure

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
-output "webapp_url" {
-    value = { for k, v in azurerm_linux_web_app.app: k => "https://${v.name}.azurewebsites.net"}
+output "frontend_url" {
+  value       = "https://${azurerm_static_site.frontend.default_host_name}"
+  description = "React app URL"
+}
+
+output "backend_url" {
+  value       = "https://${azurerm_linux_web_app.backend.default_hostname}"
+  description = "Go API URL"
+}
+
+output "static_web_app_name" {
+  value       = azurerm_static_site.frontend.name
+  description = "For deployment token"
+}
+
+output "app_service_name" {
+  value       = azurerm_linux_web_app.backend.name
+  description = "For publish profile"
 }
