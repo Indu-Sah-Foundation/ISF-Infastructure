fd99386701b79915f070c916fc1013a7ea345740
exploratoryprorammer <rohan.k.sah@gmail.com>
2025-11-11 17:08:03 -0600

    added docker resources

--- main.tf
+++ main.tf
@@ -23,6 +23,14 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_container_registry" "acr" {
+  name                = "${replace(var.app_name, "-", "")}acr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
 resource "azurerm_service_plan" "asp" {
   name                = "${var.app_name}-plan"
   resource_group_name = azurerm_resource_group.rg.name
@@ -36,19 +44,55 @@ resource "azurerm_linux_web_app" "backend" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.asp.id
+  https_only          = true
 
   site_config {
+    always_on = true
+    
     application_stack {
-      go_version = "1.19"
+      docker_registry_url      = "https://${azurerm_container_registry.acr.login_server}"
+      docker_registry_username = azurerm_container_registry.acr.admin_username
+      docker_registry_password = azurerm_container_registry.acr.admin_password
+      docker_image_name        = "go-backend:latest"
     }
+
     cors {
-      allowed_origins = ["*"]
+      allowed_origins     = ["https://${azurerm_static_web_app.frontend.default_host_name}"]
+      support_credentials = false
     }
+
+    health_check_path = "/health"
   }
 
   app_settings = {
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
-    "WEBSITES_PORT"                  = "8080"
+    "WEBSITES_PORT"                   = "8080"
+    "DOCKER_REGISTRY_SERVER_URL"      = "https://${azurerm_container_registry.acr.login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
+    "PORT"                            = "8080"
+    "GIN_MODE"                        = "release"
+    # MONGODB_URI will be set by GitHub Actions (secrets shouldn't be in IaC)
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  logs {
+    application_logs {
+      file_system_level = "Information"
+    }
+    http_logs {
+      file_system {
+        retention_in_days = 7
+        retention_in_mb   = 35
+      }
+    }
+  }
+
+  tags = {
+    Environment = "Production"
+    ManagedBy   = "Terraform"
   }
 }
 
