e6615b9607496847936bc3a800f92e3ae5ceae12
exploratoryprorammer <rohan.k.sah@gmail.com>
2026-05-02 22:35:32 -0500

    perf: westus2 => westus

--- postgres.tf
+++ postgres.tf
@@ -1,5 +1,5 @@
 resource "azurerm_postgresql_flexible_server" "db" {
-  name                   = "${var.app_name}-postgres-west"
+  name                   = "${var.app_name}-postgres"
   resource_group_name    = azurerm_resource_group.rg.name
   location               = "${var.postgres_location}"
   version                = "16"
