0c57a48ca3d1a45d4cb0cc6d897d6b4b53a36a86
exploratoryprorammer <rohan.k.sah@gmail.com>
2026-05-11 01:29:54 -0500

    infra: clean KV refs, route DB password through KV, slim CI surface

--- keyvault.tf
+++ keyvault.tf
@@ -64,47 +64,79 @@ resource "azurerm_key_vault_access_policy" "github_actions" {
 data "azurerm_key_vault_secret" "jwt_secret" {
   name         = "jwt-secret"
   key_vault_id = azurerm_key_vault.kv.id
-  depends_on   = [azurerm_key_vault_access_policy.developer]
+  depends_on = [
+    azurerm_key_vault_access_policy.developer,
+    azurerm_key_vault_access_policy.github_actions,
+    azurerm_key_vault_access_policy.app_service,
+  ]
 }
 
 data "azurerm_key_vault_secret" "stripe_secret_key" {
   name         = "stripe-secret-key"
   key_vault_id = azurerm_key_vault.kv.id
-  depends_on   = [azurerm_key_vault_access_policy.developer]
+  depends_on = [
+    azurerm_key_vault_access_policy.developer,
+    azurerm_key_vault_access_policy.github_actions,
+    azurerm_key_vault_access_policy.app_service,
+  ]
 }
 
 data "azurerm_key_vault_secret" "stripe_webhook_secret" {
   name         = "stripe-webhook-secret"
   key_vault_id = azurerm_key_vault.kv.id
-  depends_on   = [azurerm_key_vault_access_policy.developer]
+  depends_on = [
+    azurerm_key_vault_access_policy.developer,
+    azurerm_key_vault_access_policy.github_actions,
+    azurerm_key_vault_access_policy.app_service,
+  ]
 }
 
 data "azurerm_key_vault_secret" "admin_password" {
   name         = "admin-password"
   key_vault_id = azurerm_key_vault.kv.id
-  depends_on   = [azurerm_key_vault_access_policy.developer]
+  depends_on = [
+    azurerm_key_vault_access_policy.developer,
+    azurerm_key_vault_access_policy.github_actions,
+    azurerm_key_vault_access_policy.app_service,
+  ]
 }
 
 data "azurerm_key_vault_secret" "database_url" {
   name         = "database-url"
   key_vault_id = azurerm_key_vault.kv.id
-  depends_on   = [azurerm_key_vault_access_policy.developer]
+  depends_on = [
+    azurerm_key_vault_access_policy.developer,
+    azurerm_key_vault_access_policy.github_actions,
+    azurerm_key_vault_access_policy.app_service,
+  ]
 }
 
 data "azurerm_key_vault_secret" "redis_url" {
   name         = "redis-url"
   key_vault_id = azurerm_key_vault.kv.id
-  depends_on   = [azurerm_key_vault_access_policy.developer]
+  depends_on = [
+    azurerm_key_vault_access_policy.developer,
+    azurerm_key_vault_access_policy.github_actions,
+    azurerm_key_vault_access_policy.app_service,
+  ]
 }
 
 data "azurerm_key_vault_secret" "translator_key" {
   name         = "translator-key"
   key_vault_id = azurerm_key_vault.kv.id
-  depends_on   = [azurerm_key_vault_access_policy.developer]
+  depends_on = [
+    azurerm_key_vault_access_policy.developer,
+    azurerm_key_vault_access_policy.github_actions,
+    azurerm_key_vault_access_policy.app_service,
+  ]
 }
 
 data "azurerm_key_vault_secret" "db_admin_password" {
   name         = "db-admin-password"
   key_vault_id = azurerm_key_vault.kv.id
-  depends_on   = [azurerm_key_vault_access_policy.developer]
+  depends_on = [
+    azurerm_key_vault_access_policy.developer,
+    azurerm_key_vault_access_policy.github_actions,
+    azurerm_key_vault_access_policy.app_service,
+  ]
 }
